1da13b0435d32142a897f153872ce3332540d8ba
geeshow <kute80@naver.com>
2020-07-22 00:25:27 +0900

    impl list movies

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
 
@@ -13,10 +14,12 @@ function LandingPage() {
         fetch(endpoint)
             .then(response => response.json())
             .then(response => {
-                setMovies([response.results])
+                console.log(response.results)
+                setMovies(response.results)
                 setMainMovieImage(response.results[0])
             })
     }, [])
+
     return (
         <div style={{ width: '100%', margin: '0' }}>
 
@@ -31,6 +34,19 @@ function LandingPage() {
                 <h2>Movies by latest</h2>
                 <hr />
 
+                <Row gutter={[16, 16]}>
+
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                image={movie.poster_path ?
+                                `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center' }}></div>
