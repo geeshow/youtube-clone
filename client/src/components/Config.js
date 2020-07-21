5c40e1f15cdf0b63164e9cb68b7f12880a94177d
geeshow <kute80@naver.com>
2020-07-21 23:28:24 +0900

    Impl MainImage

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,6 +2,6 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
+export const API_KEY = 'd0f7cc4427aa2919da3744294a64b941';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 
